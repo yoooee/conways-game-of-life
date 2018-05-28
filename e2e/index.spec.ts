8a5ce5138d463780c8df59472b8993e0733375df
Josef Staroba <jstaroba@centene.com>
2018-05-28 17:00:59 -0500

    Update header/welcome message

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -5,14 +5,6 @@ describe('homepage', () => {
     browser.get('/');
 
     const h1 = await $('h1');
-    expect(h1.getText()).toEqual('Welcome');
+    expect(h1.getText()).toEqual('Conways Game of Life');
   });
-  //it('shows the welcome message', () => {
-  //browser.get('/');
-  //let h1 = $('h1');
-  //h1.getText().then((text) => {
-  //console.log('.....', text);
-  //})
-  //console.log('welcome message', $('h1').getText());
-  //})
 });
